ada210299ed231ae787c705092bf5f2ca02e6779
Kenta Sato <bicycle1885@gmail.com>
2015-05-31 04:30:12 +0900

    add constructor and rank

--- src/WaveletMatrices.jl
+++ src/WaveletMatrices.jl
@@ -1,5 +1,81 @@
 module WaveletMatrices
 
-# package code goes here
+export WaveletMatrix, rank
+
+using IndexableDicts
+import IndexableDicts: rank
+
+immutable WaveletMatrix{B<:AbstractBitVector}
+    bits::Vector{B}
+    nzeros::Vector{Int}
+end
+
+function WaveletMatrix(data::Vector{Uint8})
+    n = length(data)
+    bits = SuccinctBitVector[]
+    nzeros = Int[]
+    # TODO: efficient construction
+    data′ = Array(Uint8, n)
+    for d in 1:8
+        # scan d-th bit
+        bits′ = SuccinctBitVector()
+        for i in 1:n
+            if bitat(data[i], 8 - d + 1)
+                # right
+                push!(bits′, 1)
+            else
+                # left
+                push!(bits′, 0)
+            end
+        end
+        nzero = rank0(bits′, n)
+        l = r = 1
+        for i in 1:n
+            if bits′[i]
+                # right
+                data′[nzero+r] = data[i]
+                r += 1
+            else
+                # left
+                data′[l] = data[i]
+                l += 1
+            end
+        end
+        push!(bits, bits′)
+        push!(nzeros, nzero)
+        copy!(data, data′)
+    end
+    WaveletMatrix(bits, nzeros)
+end
+
+WaveletMatrix(str::ByteString) = WaveletMatrix(copy(str.data))
+
+function rank(a::Uint8, wm::WaveletMatrix, i::Int)
+    sp = 0
+    ep = i
+    # scan from the most significant bit to the least significant bit
+    # e.g.
+    #   0b01000101
+    #     =======>
+    #   d=1 .... 8
+    for d in 1:8
+        bit = bitat(a, 8 - d + 1)
+        #@show bit sp:ep
+        sp = rank(bit, wm.bits[d], sp)
+        ep = rank(bit, wm.bits[d], ep)
+        if bit
+            sp += wm.nzeros[d]
+            ep += wm.nzeros[d]
+        end
+        #@show sp:ep
+    end
+    return ep - sp
+end
+
+rank(c::Char, wm::WaveletMatrix, i::Int) = rank(convert(Uint8, c), wm, i)
+
+function bitat(byte::Uint8, i::Int)
+    return ((byte >> (i-1)) & 0x01) == 1
+end
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,82 @@
 using WaveletMatrices
+using IndexableDicts
 using Base.Test
+using FactCheck
 
-# write your own tests here
-@test 1 == 1
+srand(12345)
+
+facts("WaveletMatrix") do
+    context("ordered") do
+        wm = WaveletMatrix([0x00, 0x01, 0x02, 0x03])
+        # 0x00
+        @fact rank(0x00, wm, 1) => 1
+        @fact rank(0x00, wm, 2) => 1
+        @fact rank(0x00, wm, 3) => 1
+        @fact rank(0x00, wm, 4) => 1
+        # 0x01
+        @fact rank(0x01, wm, 1) => 0
+        @fact rank(0x01, wm, 2) => 1
+        @fact rank(0x01, wm, 3) => 1
+        @fact rank(0x01, wm, 4) => 1
+        # 0x02
+        @fact rank(0x02, wm, 1) => 0
+        @fact rank(0x02, wm, 2) => 0
+        @fact rank(0x02, wm, 3) => 1
+        @fact rank(0x02, wm, 4) => 1
+        # 0x03
+        @fact rank(0x03, wm, 1) => 0
+        @fact rank(0x03, wm, 2) => 0
+        @fact rank(0x03, wm, 3) => 0
+        @fact rank(0x03, wm, 4) => 1
+    end
+
+    context("unordered") do
+        wm = WaveletMatrix([0x01, 0x03, 0x02, 0x00])
+        # 0x00
+        @fact rank(0x00, wm, 1) => 0
+        @fact rank(0x00, wm, 2) => 0
+        @fact rank(0x00, wm, 3) => 0
+        @fact rank(0x00, wm, 4) => 1
+        # 0x01
+        @fact rank(0x01, wm, 1) => 1
+        @fact rank(0x01, wm, 2) => 1
+        @fact rank(0x01, wm, 3) => 1
+        @fact rank(0x01, wm, 4) => 1
+        # 0x02
+        @fact rank(0x02, wm, 1) => 0
+        @fact rank(0x02, wm, 2) => 0
+        @fact rank(0x02, wm, 3) => 1
+        @fact rank(0x02, wm, 4) => 1
+        # 0x03
+        @fact rank(0x03, wm, 1) => 0
+        @fact rank(0x03, wm, 2) => 1
+        @fact rank(0x03, wm, 3) => 1
+        @fact rank(0x03, wm, 4) => 1
+    end
+
+    context("homogeneous") do
+        wm = WaveletMatrix([0x01, 0x01, 0x01, 0x01])
+        @fact rank(0x01, wm, 1) => 1
+        @fact rank(0x01, wm, 2) => 2
+        @fact rank(0x01, wm, 3) => 3
+        @fact rank(0x01, wm, 4) => 4
+    end
+
+    context("ASCIIString") do
+        s = "abracadabra"
+        wm = WaveletMatrix(s)
+        for char in 'a':'z', i in 1:length(s)
+            @fact rank(char, wm, i) => count(c -> c == char, s[1:i])
+        end
+    end
+
+    context("random") do
+        # there is at least one duplicated byte (pigeonhole principle)
+        len = typemax(Uint8) + 1
+        bytes = rand(Uint8, len)
+        wm = WaveletMatrix(copy(bytes))
+        for byte in 0x00:0xff, i in 1:len
+            @fact rank(byte, wm, i) => count(b -> b == byte, bytes[1:i])
+        end
+    end
+end
