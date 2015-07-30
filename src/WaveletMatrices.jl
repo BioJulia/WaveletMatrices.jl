74f448cf89ef309b1fbd1535177df868089c5d65
Kenta Sato <bicycle1885@gmail.com>
2015-07-30 21:15:39 +0900

    fix type

--- src/WaveletMatrices.jl
+++ src/WaveletMatrices.jl
@@ -7,25 +7,25 @@ import Base: endof, length, sizeof, getindex
 using IndexableBitVectors
 import IndexableBitVectors: rank
 
-immutable WaveletMatrix{B<:AbstractBitVector,N}
-    bits::NTuple{N,B}
-    nzeros::NTuple{N,Int}
+immutable WaveletMatrix{n,B<:AbstractBitVector}
+    bits::NTuple{n,B}
+    nzeros::NTuple{n,Int}
     len::Int
 end
 
-function WaveletMatrix{B<:AbstractBitVector,T<:Unsigned}(::Type{B}, data::Vector{T}, N::Int)
-    @assert 1 ≤ N ≤ sizeof(T) * 8
+function WaveletMatrix{B<:AbstractBitVector,T<:Unsigned}(::Type{B}, data::Vector{T}, n::Int)
+    @assert 1 ≤ n ≤ sizeof(T) * 8
     len = length(data)
     bits = B[]
     nzeros = Int[]
     # TODO: efficient construction
     data = copy(data)
     data′ = Array(T, len)
-    for d in 1:N
+    for d in 1:n
         # scan d-th bit
         bits′ = B()
         for i in 1:len
-            if (data[i] >> (N - d)) & 1 == 1
+            if (data[i] >> (n - d)) & 1 == 1
                 # right
                 push!(bits′, 1)
             else
@@ -50,43 +50,43 @@ function WaveletMatrix{B<:AbstractBitVector,T<:Unsigned}(::Type{B}, data::Vector
         push!(nzeros, nzero)
         copy!(data, data′)
     end
-    return WaveletMatrix{CompactBitVector,N}(tuple(bits...), tuple(nzeros...), len)
+    return WaveletMatrix{n,B}(tuple(bits...), tuple(nzeros...), len)
 end
 
-WaveletMatrix{T}(src::Vector{T}, N::Int=sizeof(T) * 8) = WaveletMatrix(CompactBitVector, src, N)
+WaveletMatrix{T<:Unsigned}(src::Vector{T}, n::Int=sizeof(T) * 8) = WaveletMatrix(CompactBitVector, src, n)
 WaveletMatrix(str::ByteString) = WaveletMatrix(str.data)
 
 endof(wm::WaveletMatrix)  = wm.len
 length(wm::WaveletMatrix) = wm.len
 
-function sizeof{B,N}(wm::WaveletMatrix{B,N})
+function sizeof{n}(wm::WaveletMatrix{n})
     s = 0
     # bits
-    for n in 1:N
-        s += sizeof(wm.bits[n])
+    for d in 1:n
+        s += sizeof(wm.bits[d])
     end
     # nzeros
-    s += N * sizeof(Int)
+    s += n * sizeof(Int)
     # len
     s += sizeof(Int)
     return s
 end
 
-function getindex{B,N}(wm::WaveletMatrix{B,N}, i::Integer)
+function getindex{n}(wm::WaveletMatrix{n}, i::Integer)
     ret = zero(UInt64)
-    for d in 1:N
+    @inbounds for d in 1:n
         bit = wm.bits[d][i]
         if bit
             i = wm.nzeros[d] + rank1(wm.bits[d], i)
         else
             i = rank0(wm.bits[d], i)
         end
-        ret |= convert(UInt64, bit) << (N - d)
+        ret |= convert(UInt64, bit) << (n - d)
     end
     return ret
 end
 
-function rank{B<:AbstractBitVector,N}(a::Unsigned, wm::WaveletMatrix{B,N}, i::Int)
+function rank{n}(a::Unsigned, wm::WaveletMatrix{n}, i::Int)
     @assert 0 ≤ i ≤ endof(wm)
     if i == 0
         return 0
@@ -98,8 +98,8 @@ function rank{B<:AbstractBitVector,N}(a::Unsigned, wm::WaveletMatrix{B,N}, i::In
     #   0b01000101
     #     =======>
     #   d=1 .... 8
-    @inbounds for d in 1:N
-        bit = (a >> (N - d)) & 1 == 1
+    @inbounds for d in 1:n
+        bit = (a >> (n - d)) & 1 == 1
         sp = rank(bit, wm.bits[d], sp)
         ep = rank(bit, wm.bits[d], ep)
         if bit
